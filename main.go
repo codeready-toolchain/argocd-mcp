d06930a0c893a8b956bbc2c16aaa3462f4359787
Xavier Coulon <xcoulon@redhat.com>
2025-09-15 14:01:48 +0200

    refactor: flag for Argo CD token
    pass the value via a flag, instead of specifying the file where the value is stored
    include env var expansion when the value is in the form of `` or ``
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- main.go
+++ main.go
@@ -4,24 +4,19 @@ import (
 	"log/slog"
 	"os"
 
-	flag "github.com/spf13/pflag"
-
 	"github.com/xcoulon/argocd-mcp/internal/argocdmcp"
+	"github.com/xcoulon/argocd-mcp/internal/configuration"
 	"github.com/xcoulon/converse-mcp/pkg/server"
 )
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{}))
-	url := flag.String("argocd-url", "", "URL of the Argo CD server to query")
-	tokenFile := flag.String("argocd-token-file", "", "File with the token to query Argo CD")
-	insecure := flag.Bool("insecure", false, "Allow insecure TLS connections")
-	flag.Parse()
-	token, err := os.ReadFile(*tokenFile)
+	cfg, err := configuration.New()
 	if err != nil {
-		logger.Error("failed to read token file", "error", err)
+		logger.Error("failed to load configuration", "error", err.Error())
 		os.Exit(1)
 	}
-	b := argocdmcp.NewHTTPClient(*url, string(token), *insecure)
+	b := argocdmcp.NewHTTPClient(cfg.URL, cfg.Token, cfg.Insecure)
 	srv := argocdmcp.NewServer(logger, b)
 	srv.Start(server.StdioChannel)
 	logger.Info("server started")
