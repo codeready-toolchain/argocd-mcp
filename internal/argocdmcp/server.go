29fdc9808b05665217acc13116830ce1fdbbd317
Xavier Coulon <xcoulon@redhat.com>
2025-09-17 10:39:50 +0200

    refactor: sub-command to start with 'stdio' transport
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- internal/argocdmcp/server.go
+++ internal/argocdmcp/server.go
@@ -2,17 +2,13 @@ package argocdmcp
 
 import (
 	"log/slog"
-	"os"
 
 	"github.com/creachadair/jrpc2"
-	"github.com/creachadair/jrpc2/channel"
 	mcpserver "github.com/xcoulon/converse-mcp/pkg/server"
 )
 
-var StdioChannel = channel.Line(os.Stdin, os.Stdout)
-
 func NewServer(logger *slog.Logger, cl HTTPClient) *jrpc2.Server {
-	mux := mcpserver.NewMux("argocd-mcp", "0.1").
+	mux := mcpserver.NewMux("argocd-mcp", "0.1", logger).
 		WithPrompt(UnhealthyResourcesPrompt, UnhealthyApplicationResourcesPromptHandle(logger, cl)).
 		WithTool(UnhealthyApplicationsTool, UnhealthyApplicationsToolHandle(logger, cl)).
 		WithTool(UnhealthyApplicationResourcesTool, UnhealthyApplicationResourcesToolHandle(logger, cl)).
